5769107c62aa848f8fcbf0d1b58b8ce2c2c3d169
punch5545 <punch5545@naver.com>
2019-03-15 03:24:38 +0900

    d
    중복제거용 1차원배열과 ptr 추가
    코드 간소화
    빙고체크 자료형 변경

--- BingoGame_SGA/Project1/Main.cpp
+++ BingoGame_SGA/Project1/Main.cpp
@@ -3,53 +3,59 @@
 #include <time.h>
 #include <stdlib.h>
 
-bool bingoCheck();
+int bingoCheck();
 void playGround(int num);
 int bingo[3][3]{0,0,0,
 				0,0,0,
 				0,0,0};
 int hide[3][3];
+int bingoCount;
 bool bCheck[8];
 
 int main(void) {
 
 	int inputNum = 0;
+	int list[9];
 	int temp;
+	int * listptr = list;
 	srand(time(0));
-	int bingoCount = 0;
 
 	for (int i = 0; i < 8; i++) {
 		bCheck[i] = false;
 	}
 
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 3; j++) {
-		//jump:
-			bingo[i][j] = rand() % 30 + 1;
-			
+	for (int i = 0; i < 9; i++)	{
+	jump:
+		int temp = rand() % 30 + 1;
+		for (int j = 0; j < i; j++)	{
+			if (temp == list[j]){
+				goto jump;
+			}
 		}
+		list[i] = temp;
 	}
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 3; j++) {
+	temp = 0;
+
+	for (int i = 0; i < 3; i++){
+		for (int j = 0; j < 3; j++)	{
+			bingo[i][j] = *(listptr + temp++);
 			hide[i][j] = '*';
 			printf(" %c ", hide[i][j]);
 		}
 		printf("\n\n");
 	}
+
 	while (true)
 	{
 		printf("입력 (1~30)> ");
 		scanf("%d", &inputNum);
 		playGround(inputNum);
-		bool check = bingoCheck();
-		if (check) {
-			bingoCount++;
-			if (bingoCount>2) {
-				printf("클리어!\n");
-			}
-			else {
-				printf("현재 %d빙고입니다.\n", bingoCount);
-			}
+		bingoCheck();
+		if (bingoCount>2) {
+			printf("클리어!\n");
+		}
+		else {
+			printf("현재 %d빙고입니다.\n", bingoCount);
 		}
 	}	
 }
@@ -81,25 +87,25 @@ void playGround(int num) {
 	}
 }
 
-bool bingoCheck() {
+int bingoCheck() {
 	for (int i = 0; i < 3; i++) {
 		if (hide[i][0] != '*' && hide[i][1] != '*' && hide[i][2] != '*' && !bCheck[i]) {
 			bCheck[i] = true;
-			return true;
+			bingoCount++;
 		}
 			
 		if (hide[0][i] != '*' && hide[1][i] != '*' && hide[2][i] != '*' && !bCheck[i+3]) {
 			bCheck[i + 3] = true;
-			return true;
+			bingoCount++;
 		}
 	}
 	if (hide[0][0] != '*' && hide[1][1] != '*' && hide[2][2] != '*' && !bCheck[7]) {
 		bCheck[7] = true;
-		return true;
+		bingoCount++;
 	}
 	if (hide[2][0] != '*' && hide[1][1] != '*' && hide[0][2] != '*' && !bCheck[6]) {
 		bCheck[6] = true;
-		return true;
+		bingoCount++;
 	}
-	return false;
+		return bingoCount;
 }
